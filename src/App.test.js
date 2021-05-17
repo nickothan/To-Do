2f6b5dbba105bf77f013433724850577e9aef173
Revi <olliv@outlook.com>
2021-05-17 13:05:10 +0200

    Update test of App component

--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
+import React from 'react'
+import { render } from '@testing-library/react'
+import { Provider } from 'react-redux'
+import { store } from './app/store'
+import App from './App'
 
-test('renders learn react link', () => {
-  const { getByText } = render(
+test('render App', () => {
+  const { container, getByText } = render(
     <Provider store={store}>
       <App />
     </Provider>
-  );
+  )
 
-  expect(getByText(/learn/i)).toBeInTheDocument();
-});
+  expect(getByText(/TO DO/i)).toBeInTheDocument()
+  expect(container).toMatchSnapshot()
+})
