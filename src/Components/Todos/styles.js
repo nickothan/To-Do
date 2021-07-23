e411ef097794869604f5239cb70f14d328c09086
Nickothan <s1nicolasrg@gmail.com>
2021-07-22 22:10:27 -0500

    styles list

--- src/Components/Todos/styles.js
+++ src/Components/Todos/styles.js
@@ -1,10 +1,18 @@
-import styled from "styled-components/macro";
+import styled from 'styled-components/macro'
 
 export const ToDoContainer = styled.ul`
-    max-width: 400px;
-    margin: 50px auto;
-    padding: 20px;
-    border-radius: 3px;
-    box-shadow: 0 0 3px blue;
-    list-style: none;
-`;
+  max-width: 400px;
+  margin: 50px auto;
+  padding: 20px;
+  background-color: rgb(255, 255, 204);
+  box-shadow: yellow 0px 0px 5px 2px;
+  border-radius: 3px;
+  border: 1 solid black;
+
+  list-style: none;
+
+  h4 {
+    width: 100%;
+    overflow: hidden;
+  }
+`
