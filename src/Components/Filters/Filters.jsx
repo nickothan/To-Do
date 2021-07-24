f89aa35479686fa5b5d51793fa4900c0e1977a32
Revi <oliiv@outlook.com>
2021-07-24 13:31:01 +0200

    [TAD-10] Update Styles (#6)
    Co-authored-by: Revi <oliver.ramirez@devbase.us>

--- src/Components/Filters/Filters.jsx
+++ src/Components/Filters/Filters.jsx
@@ -1,34 +1,36 @@
-import { useDispatch } from 'react-redux'
-import { setFilter } from '../../app/Slice/filterSlice'
-import { filters } from '../../consts'
+import { useDispatch } from "react-redux";
+import { setFilter } from "../../app/Slice/filterSlice";
+import { filters } from "../../consts";
 
-import { FilterContainer } from './styles'
+import { FilterContainer } from "./styles";
 
 export default function Filter() {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   return (
     <FilterContainer>
       {[
         {
           filter: filters.ALL,
-          label: 'All'
+          label: "All",
         },
         {
           filter: filters.IN_PROGRESS,
-          label: 'In progress'
+          label: "In progress",
         },
         {
           filter: filters.COMPLETED,
-          label: 'Completed'
-        }
+          label: "Completed",
+        },
       ].map(({ filter, label }) => (
         <button
+          className="btn"
           key={Math.random() * 3}
-          onClick={() => dispatch(setFilter(filter))}>
+          onClick={() => dispatch(setFilter(filter))}
+        >
           {label}
         </button>
       ))}
     </FilterContainer>
-  )
+  );
 }
