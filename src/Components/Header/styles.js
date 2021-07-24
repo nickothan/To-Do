02f63aff311074e0ce01b22d1b46f01024bc1eb5
Revi <oliver.ramirez@devbase.us>
2021-07-24 13:28:45 +0200

    Update Styles

--- src/Components/Header/styles.js
+++ src/Components/Header/styles.js
@@ -1,8 +1,7 @@
 import styled from "styled-components/macro";
 
 export const Content = styled.div`
-    margin: 0;
-    padding: 0.5rem;
-    background-color: royalblue;
-    text-align: center;
+  margin: 0;
+  padding: 0.5rem;
+  text-align: center;
 `;
